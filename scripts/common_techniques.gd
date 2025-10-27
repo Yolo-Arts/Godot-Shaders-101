898b776661bcdf8bfc9a0e6bbc6c5992db7adf73
Yolo-Arts <Jackteong123@gmail.com>
2025-10-27 17:27:38 +0000

    added dissolve animation

--- scripts/common_techniques.gd
+++ scripts/common_techniques.gd
@@ -1,1 +1,6 @@
 extends Node2D
+@onready var animation_player = $dissolve/AnimationPlayer
+
+#func _process(delta):
+	#if Input.is_action_just_pressed("flash"):
+		#animation_player.play("dissolve")
